953a322d91abb16c69bd852ea7a034fd4ab29af2
sohilsheth21 <78338111+sohilsheth21@users.noreply.github.com>
2021-05-24 19:50:16 +0530

    Update pygame.py

--- pygame.py
+++ pygame.py
@@ -6,8 +6,7 @@ pygame.init()
 win=pygame.display.set_mode((500,500))
 
 
-
-print('Connecting to vehicle on: "udp:127.0.0.1:14550" ')
+print('Connecting to vehicle on: udp:127.0.0.1:14550 ')
 vehicle = connect("udp:127.0.0.1:14550", wait_ready=True)
 print("connected successfully")
 gnd_speed=40
@@ -48,14 +47,14 @@ def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
     Move vehicle in direction based on specified velocity vectors.
     """
     msg = vehicle.message_factory.set_position_target_local_ned_encode(
-        0,       # time_boot_ms (not used)
-        0, 0,    # target system, target component
+        0,      
+        0, 0,   
         mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
-        0b0000111111000111, # type_mask (only speeds enabled)
-        0, 0, 0, # x, y, z positions (not used)
+        0b0000111111000111, #(only speeds enabled)
+        0, 0, 0, # x, y, z positions
         velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
-        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
-        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+        0, 0, 0, # x, y, z acceleration (not supported)
+        0, 0)    # yaw, yaw_rate 
 
 
     # send command to vehicle 
